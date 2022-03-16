3cae8aba29a2b0a8720263bd1b4d398ef5375949
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-16 18:31:48 +0000

    Move up

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require('net');
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+  conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: PP');
+    conn.write('Move: up');
+  }
+  )
+
+  conn.on("data", (data) => {
+    console.log("Server outputs:", data);
+
+  });
+
+  return conn
+};
+
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
