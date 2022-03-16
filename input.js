dfe8cf1d3262ce7273b72dcb61eab82344a24a9a
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-16 20:17:43 +0000

    uses commands movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,9 +12,18 @@ const setupInput = function () {
 };
 
 
-const handleUserInput = function () {
-  process.exit();
-
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === "w") {
+    connection.write('Move: up');
+  } else if (key === "a") {
+    connection.write('Move: left');
+  } else if (key === "s") {
+    connection.write('Move: down');
+  } else if (key === "d") {
+    connection.write('Move: right');
+  }
 };
 
 module.exports = setupInput; 
